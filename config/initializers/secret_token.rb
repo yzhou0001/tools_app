765301ce5ffd8b3686a643da19aa0afa5d5e99b7
Ying Zhou <yzhou0001@yahoo.com>
2013-07-06 16:03:43 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToolsApp::Application.config.secret_token = '9c088ac0557cf6063729f3d054280c2582c23d3676f56b28029cecf59f38f03d99a3ded1ccfe43d8fb45252c10d1ebb4f238470c033c24cd94a8f8869bac3c6e'
